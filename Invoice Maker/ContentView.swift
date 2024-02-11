bd0a61d24e09ae37da7af6a7e1497037bd6f2597
Mohammadn <mohammadnajafzade@yahoo.com>
2024-02-11 18:13:07 +0000

    Initial Commit

---
+++ Invoice Maker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Invoice Maker
+//
+//  Created by Mohammad Najafzadeh on 11/02/2024.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Invoice Maker/Invoice_MakerApp.swift
@@ -0,0 +1,18 @@
+//
+//  Invoice_MakerApp.swift
+//  Invoice Maker
+//
+//  Created by Mohammad Najafzadeh on 11/02/2024.
+//
+
+import SwiftData
+import SwiftUI
+
+@main
+struct Invoice_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
