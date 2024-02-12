13a682ea670ea5aa2bbb958d6e4cf9eed8665830
Mohammadn <mohammadnajafzade@yahoo.com>
2024-02-12 19:47:01 +0000

    feat(ContentView.swift): add customers tab to TabItems enum and ContentView This change allows users to manage customers directly from the main view.

--- Invoice Maker/Views/ContentView.swift
+++ Invoice Maker/Views/ContentView.swift
@@ -8,8 +8,8 @@
 import SwiftData
 import SwiftUI
 
-enum TabItems {
-    case settings, products, invoices
+enum TabItems: Hashable {
+    case settings, products, customers, invoices
 }
 
 struct ContentView: View {
@@ -21,23 +21,28 @@ struct ContentView: View {
                 .tabItem {
                     Label("تنظیمات", systemImage: "gearshape")
                 }
+                .tag(TabItems.settings)
             ProductsView()
                 .tabItem {
                     Label("محصولات", systemImage: "list.dash")
                 }
+                .tag(TabItems.products)
             Text("مشتریان")
                 .tabItem {
                     Label("مشتریان", systemImage: "person.2")
                 }
+                .tag(TabItems.customers)
             Text("فاکتورها")
                 .tabItem {
                     Label("فاکتورها", systemImage: "doc.text")
                 }
+                .tag(TabItems.invoices)
         }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(previewContainer)
         .environment(\.layoutDirection, .rightToLeft)
 }
