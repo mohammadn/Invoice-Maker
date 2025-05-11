a84afff6bde45b75295bd4afcfb2da0375f22fed
Mohammadn <mohammadnajafzade@yahoo.com>
2025-05-11 21:09:04 +0100

    refactor(Invoice Maker): restructure CustomerView into separate detail and form views for better code organization feat(Invoice Maker): add CustomerDetailView to display detailed customer information and related invoices feat(Invoice Maker): add CustomerFormView to handle customer data editing and creation fix(Invoice Maker): remove old CustomerView implementation to avoid code duplication and confusion

---
+++ Invoice Maker/Views/Customers/CustomerView/CustomerDetailView.swift
@@ -0,0 +1,96 @@
+//
+//  CustomerDetailView.swift
+//  Invoice Maker
+//
+//  Created by Mohammad Najafzadeh on 11/05/2025.
+//
+
+import SwiftData
+import SwiftUI
+
+struct CustomerDetailView: View {
+    @Query private var invoices: [StandaloneInvoice]
+    var customer: Customer
+
+    @Binding var isEditing: Bool
+
+    init(customer: Customer, isEditing: Binding<Bool>) {
+        self.customer = customer
+        _isEditing = isEditing
+
+        let customerId = customer.id
+        let predicate = #Predicate<StandaloneInvoice> {
+            $0.customerId != nil && $0.customerId == customerId
+        }
+
+        _invoices = Query(filter: predicate, sort: \.createdDate, order: .reverse)
+    }
+
+    var body: some View {
+        List {
+            Section {
+                LabeledContent("نام", value: customer.name)
+                LabeledContent("جزئیات", value: customer.details?.isEmpty == false ? customer.details! : "-")
+            }
+
+            Section {
+                LabeledContent("تلفن", value: customer.phone?.toPersian() ?? "-")
+                LabeledContent("آدرس", value: customer.address ?? "-")
+            }
+
+            Section {
+                LabeledContent("ایمیل", value: customer.email ?? "-")
+            }
+
+            Section {
+                ForEach(invoices) { invoice in
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(invoice.number)
+                                .lineLimit(1)
+                                .foregroundColor(.primary)
+
+                            Spacer()
+
+                            Text(invoice.total, format: .currency(code: "IRR"))
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                .lineLimit(1)
+                        }
+
+                        Text(invoice.date, style: .date)
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                            .lineLimit(1)
+                    }
+                }
+            } header: {
+                Text("فاکتورها")
+            } footer: {
+                if invoices.isEmpty {
+                    HStack {
+                        Spacer()
+
+                        Text("فاکتوری با نام این مشتری وجود ندارد.")
+                            .foregroundStyle(.secondary)
+
+                        Spacer()
+                    }
+                }
+            }
+        }
+        .navigationTitle(customer.name)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup {
+                Button("ویرایش") {
+                    isEditing.toggle()
+                }
+            }
+        }
+    }
+}
+
+// #Preview {
+//    CustomerDetailView()
+// }
---
+++ Invoice Maker/Views/Customers/CustomerView/CustomerView.swift
@@ -0,0 +1,31 @@
+//
+//  CustomerView.swift
+//  Invoice Maker
+//
+//  Created by Mohammad Najafzadeh on 15/04/2025.
+//
+
+import SwiftUI
+
+struct CustomerView: View {
+    @State var isEditing: Bool = false
+
+    var customer: Customer
+
+    var body: some View {
+        NavigationStack {
+            if isEditing {
+                CustomerFormView(customer: customer) {
+                    isEditing.toggle()
+                }
+            } else {
+                CustomerDetailView(customer: customer, isEditing: $isEditing)
+            }
+        }
+        .animation(.default, value: isEditing)
+    }
+}
+
+// #Preview {
+//    CustomerView()
+// }
